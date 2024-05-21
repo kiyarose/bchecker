476328e69c9028faa9c7e0dc0be1a6b93cea1d41
Damian Swan AAHS <damianmswan@icloud.com>
2024-05-21 14:05:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
+async function report(url1, url2) {
+   try{
+        fetch(url1)
+            .then(response => {
+                if (response.ok) {
+                    console.log('URL1 Accessable, Assuming internet is up!');
+                    var stat1 = 'ok'
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                alert(error);
+                var stat1 = error
+            });
+             fetch(url2)
+            .then(response => {
+                if (response.ok) {
+                    console.log('URL2 Accessable, Assuming unblocked');
+                    var stat2 = 'ok'
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                var stat2 = error
+            });
+            var results = stat1 + stat2
+            return results
+
+       }catch (error) {
+          console.log(error);
+       }
+   }
 const result = report("https://www.google.com", "https://www.xbox.com")
 document.getElementById("res").body = result
